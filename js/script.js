81cb75b3cc2c27e41fde835d7e636b1280bd5857
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2021-12-24 00:11:32 +0530

    Portfolio v2 uploaded

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","a Web Developer","a Passionate Programmer","an Open Source Enthusiast"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
